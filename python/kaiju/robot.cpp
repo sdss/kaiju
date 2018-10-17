4e13ca268d5b68645471fd42ddb6e23f1b46e68a
Conor Sayres <csayres@uw.edu>
2018-10-16 22:49:30 -0700

    working 3D version

--- python/kaiju/robot.cpp
+++ python/kaiju/robot.cpp
@@ -1,4 +1,4 @@
-// clang++ --std=c++11 robot.cpp
+// clang++ --std=c++11 -march=native robot.cpp
 #include <iostream>
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
@@ -18,20 +18,22 @@
 // http://geomalgorithms.com/a07-_distance.html#dist3D_Segment_to_Segment()
 
 // constants
-const double beta_arm_width = 5;
+const double beta_arm_width = 3;
 const double buffer_distance = beta_arm_width / 2.0;
 const double collide_dist_squared = beta_arm_width * beta_arm_width;
 const double alpha_arm_len = 7.4;
 const double beta_arm_len = 15; // mm to fiber
-const double top_collide_x1 = 8.187;
-const double beta_head_end = 19.3 - 3; // measured from
-const double top_collide_x2 = 19.3 - 3 - buffer_distance;
+// const double top_collide_x1 = 8.187;
+// const double beta_head_end = 19.3 - 3; // measured from
+// const double top_collide_x2 = 19.3 - 3 - buffer_distance;
 const double pitch = 22.4;
 const double min_reach = beta_arm_len - alpha_arm_len;
 const double max_reach = beta_arm_len + alpha_arm_len;
 
 const double ang_step = 1; //1.0; // degrees
 const int maxPathSteps = (int)(ceil(500.0/ang_step));
+
+// const int maxPathSteps = 20;
 // alpha beta array - the ordered list of moves to try
 const double ab_data[] = {
     -ang_step,  ang_step,
@@ -56,23 +58,49 @@ Eigen::Array<double, 8, 2> alphaBetaArr(ab_data);
 // segments along beta arm. 0,0,0 is point where beta
 // axis touches the (bottom of) beta arm
 
+const double b1_data[] = {0, 0, 0};
+const double b2_data[] = {0, 0, 7.60};
+const double b3_data[] = {6.12, 0, 13.85};
+const double b4_data[] = {9.54, 0, 21.90};
+const double b5_data[] = {9.54, 0, 30};
+// const double b6_data[] = {15.23, 0, 30};
+const double b6_data[] = {16.3-3.0, 0, 30};
+
+Eigen::Vector3d b1_v(b1_data);
+Eigen::Vector3d b2_v(b2_data);
+Eigen::Vector3d b3_v(b3_data);
+Eigen::Vector3d b4_v(b4_data);
+Eigen::Vector3d b5_v(b5_data);
+Eigen::Vector3d b6_v(b6_data);
+
+const int betaArmPoints = 5;
+
+typedef std::array<Eigen::Vector3d, betaArmPoints> betaGeometry;
+betaGeometry betaNeutral = {b2_v, b3_v, b4_v, b5_v, b6_v};
+
+const double fiberNeutral_data[] = {beta_arm_len, 0, 0};
+Eigen::Vector3d fiber_neutral(fiberNeutral_data);
 
 
-const double beta_pt1_data[] = {0, 0, 7.60};
-Eigen::Vector3d beta_pt1(beta_pt1_data);
-const double beta_pt2_data[] = {6.12, 0, 13.85};
-Eigen::Vector3d beta_pt2(beta_pt2_data);
-const double beta_pt3_data[] = {9.54, 0, 21.90};
-Eigen::Vector3d beta_pt3(beta_pt3_data);
-const double beta_pt4_data[] = {9.54, 0, 30};
-Eigen::Vector3d beta_pt4(beta_pt4_data);
-const double beta_pt5_data[] = {15.23, 0, 30};
-Eigen::Vector3d beta_pt5(beta_pt5_data);
+// create a vector that translates from beta orgin to alpha origin
+// this is just the length of the alpha arm
+const double alpha_trans_data[] = {alpha_arm_len, 0, 0};
+Eigen::Vector3d alphaTransV(alpha_trans_data);
+// Eigen::Affine3d alphaTransT(Eigen::Translation3d(alphaTransV));
 
-// add in point for fiber tips eventually?
-typedef std::array<Eigen::Vector3d, 5> betaArmOrientType;
 
-betaArmOrientType betaArmNeutral{ {beta_pt1, beta_pt2, beta_pt3, beta_pt4, beta_pt5} };
+// const double beta_arm_pts[] = {
+//     0, 0, 0,
+//     0, 0, 7.60,
+//     6.12, 0, 13.85,
+//     9.54, 0, 21.90,
+//     9.54, 0, 30,
+//     15.23, 0, 30
+// };
+
+// Eigen::Matrix<double, 6, 3> betaArmTemplate(beta_arm_pts);
+
+
 
 #define SMALL_NUM   0.00000001 // anything that avoids division overflow
 // dot product (3D) which allows vector operations in arguments
@@ -82,20 +110,20 @@ betaArmOrientType betaArmNeutral{ {beta_pt1, beta_pt2, beta_pt3, beta_pt4, beta_
 // dist3D_Segment_to_Segment(): get the 3D minimum distance between 2 segments
 //    Input:  two 3D line segments S1 and S2
 //    Return: the shortest distance between S1 and S2
-float
+double
 dist3D_Segment_to_Segment(Eigen::Vector3d S1_P0, Eigen::Vector3d S1_P1, Eigen::Vector3d S2_P0, Eigen::Vector3d S2_P1)
 {
-    Vector   u = S1_P1 - S1_P0;
-    Vector   v = S2_P1 - S2_P0;
-    Vector   w = S1_P0 - S2_P0;
-    float    a = u.dot(u);         // always >= 0
-    float    b = u.dot(v);
-    float    c = v.dot(v);         // always >= 0
-    float    d = u.dot(w);
-    float    e = v.dot(w);
-    float    D = a*c - b*b;        // always >= 0
-    float    sc, sN, sD = D;       // sc = sN / sD, default sD = D >= 0
-    float    tc, tN, tD = D;       // tc = tN / tD, default tD = D >= 0
+    Eigen::Vector3d   u = S1_P1 - S1_P0;
+    Eigen::Vector3d   v = S2_P1 - S2_P0;
+    Eigen::Vector3d   w = S1_P0 - S2_P0;
+    double    a = u.dot(u);         // always >= 0
+    double    b = u.dot(v);
+    double    c = v.dot(v);         // always >= 0
+    double    d = u.dot(w);
+    double    e = v.dot(w);
+    double    D = a*c - b*b;        // always >= 0
+    double    sc, sN, sD = D;       // sc = sN / sD, default sD = D >= 0
+    double    tc, tN, tD = D;       // tc = tN / tD, default tD = D >= 0
 
     // compute the line parameters of the two closest points
     if (D < SMALL_NUM) { // the lines are almost parallel
@@ -148,14 +176,14 @@ dist3D_Segment_to_Segment(Eigen::Vector3d S1_P0, Eigen::Vector3d S1_P1, Eigen::V
     tc = (abs(tN) < SMALL_NUM ? 0.0 : tN / tD);
 
     // get the difference of the two closest points
-    Vector   dP = w + (sc * u) - (tc * v);  // =  S1(sc) - S2(tc)
+    Eigen::Vector3d   dP = w + (sc * u) - (tc * v);  // =  S1(sc) - S2(tc)
 
     return dP.dot(dP);   // return the closest distance squared
 }
 
 double randomSample(){
     // return between 0 and 1
-    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    return static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
 }
 
 std::array<double, 2> alphaBetaFromXY(double x, double y){
@@ -238,12 +266,16 @@ Eigen::MatrixXd getHexPositions(int nDia){
 }
 
 class Robot {
-private:
-    std::array<double, 2> getXYAlongBeta(double);
+// private:
+//     std::array<double, 2> getXYAlongBeta(double);
 public:
     int id;
     double xPos, yPos, alpha, beta;
-    betaArmOrientType betaOrientation;
+    Eigen::Affine3d betaRot, alphaRot;
+    Eigen::Vector3d fiber_XYZ;
+    Eigen::Vector3d transXY;
+    betaGeometry betaOrientation;
+    std::array<Eigen::Vector3d, betaArmPoints-1> betaVec
     std::array<double, 2> xyTarget, xyAlphaArm;
     std::vector<double> alphaPath, betaPath;
     std::list<Robot *> neighbors;
@@ -254,7 +286,7 @@ public:
     void topCollideZone();
     void bottomCollideZone();
     bool isCollided();
-    bool isTopCollided();
+    // bool isTopCollided();
     // bool isBottomCollided();
     void decollide();
     void setXYUniform();
@@ -265,6 +297,7 @@ public:
 Robot::Robot(int myid, double myxPos, double myyPos) {
     xPos = myxPos;
     yPos = myyPos;
+    transXY = Eigen::Vector3d(myxPos, myyPos, 0);
     id = myid;
 }
 
@@ -283,59 +316,68 @@ void Robot::addNeighbor(Robot * rNeighbor){
     neighbors.push_back(rNeighbor);
 }
 
-std::array<double, 2> Robot::getXYAlongBeta(double xBeta){
-    // return xy global position from a distance along beta arm
-    double y = 0;
-    double x = xBeta;
+// std::array<double, 2> Robot::getXYAlongBeta(double xBeta){
+//     // return xy global position from a distance along beta arm
+//     double y = 0;
+//     double x = xBeta;
 
-    // first rotate about beta
-    double x_b = cosBeta * x - sinBeta * y;
-    double y_b = sinBeta * x + cosBeta * y;
+//     // first rotate about beta
+//     double x_b = cosBeta * x - sinBeta * y;
+//     double y_b = sinBeta * x + cosBeta * y;
 
-    // offset by alpha arm length
-    x_b += alpha_arm_len;
+//     // offset by alpha arm length
+//     x_b += alpha_arm_len;
 
-    // next rotate about alpha
-    double x_a = cosAlpha * x_b - sinAlpha * y_b;
-    double y_a = sinAlpha * x_b + cosAlpha * y_b;
+//     // next rotate about alpha
+//     double x_a = cosAlpha * x_b - sinAlpha * y_b;
+//     double y_a = sinAlpha * x_b + cosAlpha * y_b;
 
-    // offset by robot's zero position
-    x_a += xPos;
-    y_a += yPos;
+//     // offset by robot's zero position
+//     x_a += xPos;
+//     y_a += yPos;
 
-    std::array<double, 2> outArr;
-    outArr[0] = x_a;
-    outArr[1] = y_a;
-    return outArr;
-}
+//     std::array<double, 2> outArr;
+//     outArr[0] = x_a;
+//     outArr[1] = y_a;
+//     return outArr;
+// }
 
 void Robot::setAlphaBeta(double newAlpha, double newBeta){
     alpha = newAlpha;
     beta = newBeta;
+    double alphaRad = alpha * M_PI / 180.0;
+    double betaRad = beta * M_PI / 180.0;
 
-    Eigen::AngleAxis<float> rotBeta(betaRad, Eigen::Vector3f::UnitZ());
-    Eigen::
-    for (int ii=0; ii<betaArmNeutral.size(); ii++){
-        betaOrientation[ii] =
+    // Eigen::Transform<float, 3, Eigen::Affine> t = Eigen::Transform<float, 3, Eigen::Affine>::Identity();
+    // t.rotate(Eigen::AngleAxisf(betaRad, Eigen::Vector3d::UnitZ()));
+
+
+    // we begin from beta arm reference frame (thats what betaNeutral is in)
+    // and we rotate beta about the origin 1st
+    betaRot = Eigen::AngleAxisd(betaRad, Eigen::Vector3d::UnitZ());
+    // next create rotation about alpha
+    alphaRot = Eigen::AngleAxisd(alphaRad, Eigen::Vector3d::UnitZ());
+    // full transform is
+    // 1. rotate about beta
+    // 2. translate by alpha arm length
+    // 3. rotate about alpha
+    // 4. translate by xy Pos of positioner
+    // result is orientation of positioner in 3d wrt origin of
+    // robot grid
+    // fullTransform = transXY*alphaRot*alphaTransT*betaRot;
+    // fullTransform = (alphaTransT*betaRot).matrix();
+
+    // std::cout << "alpha beta crap" << std::endl;
+    for (int ii = 0; ii < betaArmPoints; ii++){
+        // first rotate about beta (we start in beta ref frame)
+        // next translate in x by the alpha arm length
+        // we should be able to figure out how to compute
+        // this matrix outside this loop....
+        betaOrientation[ii] = transXY + (alphaRot * (alphaTransV + (betaRot * betaNeutral[ii])));
+        // std::cout << "test : " << betaOrientation[ii] <<  " : test\n" << std::endl;
     }
+    fiber_XYZ = transXY + (alphaRot * (alphaTransV + (betaRot * fiber_neutral)));
 
-    // precompute
-    sinAlpha = sin(alphaRad);
-    sinBeta = sin(betaRad);
-    cosAlpha = cos(alphaRad);
-    cosBeta = cos(betaRad);
-
-    // update targetXY pos (end of beta arm)
-    xyTarget = getXYAlongBeta(beta_arm_len);
-    xyAlphaArm = getXYAlongBeta(0); // could just rotate about alpha...but whatever
-
-    std::array<double, 2> tcpt1 = getXYAlongBeta(top_collide_x1);
-    std::array<double, 2> tcpt2 = getXYAlongBeta(top_collide_x2);
-
-    tcCoords[0] = tcpt1[0]; // get rid of tcCoords eventually? they get printed to files
-    tcCoords[1] = tcpt1[1];
-    tcCoords[2] = tcpt2[0];
-    tcCoords[3] = tcpt2[1];
 }
 
 void Robot::setAlphaBetaRand(){
@@ -351,21 +393,37 @@ void Robot::setXYUniform(){
 }
 
 
-
 bool Robot::isCollided(){
-    return isTopCollided();
-}
-
-bool Robot::isTopCollided(){
     bool iAmCollided = false;
     for (Robot * robot : neighbors){
-        if (dist3D_Segment_to_Segment(tcCoords, robot->tcCoords) < collide_dist_squared){
-            iAmCollided = true;
-            break;
+        for (int ii=0; ii<betaArmPoints-1; ii++){
+            double dist2 = dist3D_Segment_to_Segment(
+                    betaOrientation[ii], betaOrientation[ii+1],
+                    robot->betaOrientation[ii], robot->betaOrientation[ii+1]
+                );
+            if ( dist2 < collide_dist_squared){
+                    // std::cout << "dist " << dist2 - collide_dist_squared << std::endl;
+                    iAmCollided = true;
+                    break;
+            }
         }
     }
     return iAmCollided;
 }
+
+// bool Robot::isTopCollided(){
+//     // reimplement!!!
+//     return false;
+
+    // bool iAmCollided = false;
+    // for (Robot * robot : neighbors){
+    //     if (dist3D_Segment_to_Segment(tcCoords, robot->tcCoords) < collide_dist_squared){
+    //         iAmCollided = true;
+    //         break;
+    //     }
+    // }
+    // return iAmCollided;
+// }
 
 
 void Robot::decollide(){
@@ -476,6 +534,7 @@ RobotGrid::RobotGrid(int nDia){
     // add in robot reach to xyFocalBox
     for (int ii=0; ii<nRobots; ii++){
         Robot robot(ii, xyHexPos(ii, 0), xyHexPos(ii, 1));
+        // hack set all alpha betas home
         allRobots.push_back(robot);
 
     }
@@ -484,6 +543,7 @@ RobotGrid::RobotGrid(int nDia){
     // and initialze to random alpha betas
     for (Robot &r1 : allRobots){
         r1.setXYUniform();
+        // r1.setAlphaBeta(0,180);
         for (Robot &r2 : allRobots){
             if (r1.id==r2.id){
                 continue;
@@ -532,9 +592,9 @@ void RobotGrid::toFile(const char* filename){
         fprintf(pFile,
             "%i, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %i, %i\n",
             r.id, r.xPos, r.yPos, r.alpha, r.beta,
-            r.tcCoords[0], r.tcCoords[1], r.tcCoords[2], r.tcCoords[3],
+            -999.0, -999.0, -999.0, -999.0,
             -999.0, -999.0, -999.0, -999.0, // used to be bcCoords
-            r.isTopCollided(), 0
+            r.isCollided(), 0
         );
     }
     fclose(pFile);
@@ -548,7 +608,6 @@ void RobotGrid::pathGen(){
     int ii;
     for (ii=0; ii<maxPathSteps; ii++){
         bool allFolded = true;
-        // std::cout << "iter " << ii << std::endl;
         // char buffer[50];
         // sprintf(buffer, "step_%d.txt", ii);
         // toFile(buffer);
@@ -611,22 +670,22 @@ void doOneThread(int threadNum){
 //     }
 // }
 
-int main()
-{
-    int nThreads = 10;
-    std::thread t[10];
-    clock_t tStart;
-    clock_t tEnd;
-    tStart = clock();
-    for (int i = 0; i<10; ++i){
-        t[i] = std::thread(doOneThread, i);
-    }
-    for (int i=0; i<10; ++i){
-        t[i].join();
-    }
-    tEnd = clock();
-    std::cout << "time took: " << (double)(tEnd - tStart)/CLOCKS_PER_SEC << std::endl;
-}
+// int main()
+// {
+//     int nThreads = 10;
+//     std::thread t[10];
+//     clock_t tStart;
+//     clock_t tEnd;
+//     tStart = clock();
+//     for (int i = 0; i<10; ++i){
+//         t[i] = std::thread(doOneThread, i);
+//     }
+//     for (int i=0; i<10; ++i){
+//         t[i].join();
+//     }
+//     tEnd = clock();
+//     std::cout << "time took: " << (double)(tEnd - tStart)/CLOCKS_PER_SEC << std::endl;
+// }
 
 // int main()
 // {
@@ -647,17 +706,43 @@ int main()
 //
 // }
 
+int main()
+{
+    srand(0);
+    clock_t tStart;
+    clock_t tEnd;
+    tStart = clock();
+    RobotGrid rg = doOne();
+    tEnd = clock();
+    std::cout << "time took: " << (double)(tEnd - tStart)/CLOCKS_PER_SEC << std::endl;
+    for (Robot &robot : rg.allRobots){
+        robot.pathToFile();
+    }
+}
+
 // int main()
 // {
 //     srand(0);
-//     clock_t tStart;
-//     clock_t tEnd;
-//     tStart = clock();
-//     RobotGrid rg = doOne();
-//     tEnd = clock();
-//     std::cout << "time took: " << (double)(tEnd - tStart)/CLOCKS_PER_SEC << std::endl;
-//     for (Robot &robot : rg.allRobots){
-//         robot.pathToFile();
+//     RobotGrid rg (25);
+//     for (auto &r : rg.allRobots){
+//         std::cout << "alpha beta " << r.alpha << " " << r.beta << std::endl;
+//         std::cout << "beta orientation " << r.betaOrientation[0] << std::end;
 //     }
+//     std::cout << "n collisions " << rg.getNCollisions() << std::endl;
 // }
 
+// int main()
+// {
+//     double b1_data[] = {0, 0, 0};
+//     double b2_data[] = {1, 0.95, 1};
+//     double b3_data[] = {2, 2, 0};
+//     double b4_data[] = {1, 1, 1};
+
+//     Eigen::Vector3d b1_v(b1_data);
+//     Eigen::Vector3d b2_v(b2_data);
+//     Eigen::Vector3d b3_v(b3_data);
+//     Eigen::Vector3d b4_v(b4_data);
+
+//     double dist = dist3D_Segment_to_Segment(b1_v, b2_v, b3_v, b4_v);
+//     std::cout << "dist: " << dist << std::endl;
+// }
