5f17c1743756f59981474cc9bae8c07c504ae9fc
Conor Sayres <csayres@uw.edu>
2020-04-02 22:50:46 -0700

    v0.3 paper

--- bin/makorunVarStep.py
+++ bin/makorunVarStep.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     nProcs = 18
 
     # nTrials = 15
-    seeds = range(0, 100)
+    seeds = range(300, 500)
     nDia = [27]
     cbuff = [1.5, 2, 2.5, 3, 3.5]
     angStep = [0.05, 0.1, .25, 0.5, 0.75, 1]
@@ -27,15 +27,17 @@ if __name__ == "__main__":
     compileResults(saveDir, "allSeedsVarStep.csv")
 
 
-    for ii in range(10):
+    for ii in range(5):
         numpy.random.seed(ii)
-        df = pd.read_csv(os.path.join(saveDir, "allSeedsVarNum.csv"))
+        df = pd.read_csv(os.path.join(saveDir, "allSeedsVarStep.csv"))
         dfz = df[df["efficiency"]==0]
         if len(dfz) == 0:
             break
         repeats = []
         for ind, row in dfz.iterrows():
             seed = row["seed"]
+            if seed < 300:
+                continue # we already did this one
             nDia = 27
             angStep = row["angStep"]
             cbuff = row["collisionBuffer"]
@@ -43,10 +45,10 @@ if __name__ == "__main__":
             phobia = row["phobia"]
             repeats.append([seed, nDia, angStep, cbuff, [greed, phobia]])
         fname = os.path.join(saveDir, "repeats_%i.txt"%ii)
-        with open(fname, "r") as f:
+        with open(fname, "w") as f:
             for repeat in repeats:
                 f.write("%s\n"%str(repeat))
         p = Pool(nProcs)
         p.map(doOnePartial, repeats)
         p.close()
-        compileResults(saveDir, "allSeedsVarNum.csv")
+        compileResults(saveDir, "allSeedsVarStep.csv")
